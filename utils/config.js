ed826a1b4aaf3d7fdcb63a75249e3c48bd006283
AwsYaseen22 <aws.yaseen.22@gmail.com>
2023-01-20 22:09:52 +0300

    use async await and supertest library to test the api/notes

--- utils/config.js
+++ utils/config.js
@@ -1,9 +1,11 @@
 require('dotenv').config()
 
 const PORT = process.env.PORT
-const MONGODB_URI = process.env.MONGODB_URI
+const environment = process.env.NODE_ENV
+const MONGODB_URI = environment === 'test' ? process.env.TEST_MONGODB_URI  :process.env.MONGODB_URI
 
 module.exports = {
   MONGODB_URI,
-  PORT
+  PORT,
+  environment,
 }
